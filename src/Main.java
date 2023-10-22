fa3c856f87d76f7cb419ce3aaece8b9d29badd60
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-10-22 10:40:22 +0700

    variable

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Variable variable=new Variable();
+        System.out.println(Variable.d=5);
+        String name= variable.fullName="ngieyn hanidang";
+        System.out.println( name );
+        int total=variable.total(4,5);
+//        Đang phân vân  không biết là tạo bản sao ở đối số gắn cho tham số có đúng không hay là ngược lại
+        System.out.println(total);
+        System.out.println(Variable.CONSTANT);
+        String constant=Variable.CONSTANT;
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+//    Biến tĩnh không được lưu ở trên vùng nhớ stack hay heap mà thay vào đó nó được lưu ở metaspace khi khai báo
+//    một biến tĩnh tức là biến này thuộc về lớp và không cần tạo một đối tượng cụ thế cũng có thế gọi đến trực
+//    tiếp biến đó.
+    public  static int d;
+    public static final String CONSTANT="Đây biến hằng số không thể thay đổi được";
+//    biến CONSTANT được khai báo không thể thay đổi được và được lưu ở Metaspace
+    private static int e;
+//    Biến name có thể hiểu rằng đây là một biến thành viên biến này được lưu trữ ở đối tượng,nó là thuộc tính
+//    Của môt đối tượng và nó chỉ có thể thay đối truy cập được thông qua đối tượng đó.
+    private String name;
+    public String fullName="Nguyen thanh tung";
+    public int total(int a,int b){
+//        Biến c trong trường hợp này được gọi là biến cục bộ, được lưu ở stack khi chương trình chạy xong
+//        Chương trình sẽ tự thu gon,giải phóng biến c, biến này không truy cập được ngoài phương thức total
+//        Biến a và b có thể hiểu rằng là hai biến tham số (paramenters) phạm vị hoạt động chỉ được trong phương
+//        thức total
+
+         int c =a+b;
+         return c;
+    }
+
+}
